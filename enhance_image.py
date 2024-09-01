7ef1e7f4f3db8034c1c0a9c8bd9d120ad953ad41
SylvainGavoille <sylvain.gavoille@gmail.com>
2024-09-01 19:20:58 +0000

    featured image

--- enhance_image.py
+++ enhance_image.py
@@ -8,12 +8,12 @@ import os
 load_dotenv()
 
 # Set up your Hugging Face token
-HUGGING_FACE_TOKEN = os.getenv("HUGGING_FACE_TOKEN")
+#HUGGING_FACE_TOKEN = os.getenv("HUGGING_FACE_TOKEN")
 
 # You can set the token globally for your session
-from huggingface_hub import login
+#from huggingface_hub import login
 
-login(HUGGING_FACE_TOKEN)
+#login(HUGGING_FACE_TOKEN)
 
 pipeline = AutoPipelineForImage2Image.from_pretrained(
     "kandinsky-community/kandinsky-2-2-decoder", torch_dtype=torch.float16, use_safetensors=True
@@ -33,4 +33,6 @@ prompt = "Looking like a 25-year-old, wearing a tuxedo, with blonde hair, wearin
 
 # pass prompt and image to pipeline
 image = pipeline(prompt, image=init_image).images[0]
-make_image_grid([init_image, image], rows=1, cols=2)
+# Save the upscaled and resized image as a .webp file
+image.save("featured_image.webp", format="WEBP")
+#make_image_grid([init_image, image], rows=1, cols=2)
