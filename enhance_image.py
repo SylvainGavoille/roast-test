5113f274f6ce4d7e3e9abe747971850a4265aacb
SylvainGavoille <sylvain.gavoille@gmail.com>
2024-09-01 19:45:15 +0000

    featured image

--- enhance_image.py
+++ enhance_image.py
@@ -4,20 +4,23 @@ from diffusers import AutoPipelineForImage2Image
 from diffusers.utils import make_image_grid
 from dotenv import load_dotenv
 import os
+#import torch._dynamo
+
 
 load_dotenv()
 
 # Set up your Hugging Face token
-#HUGGING_FACE_TOKEN = os.getenv("HUGGING_FACE_TOKEN")
+HUGGING_FACE_TOKEN = os.getenv("HUGGING_FACE_TOKEN")
 
 # You can set the token globally for your session
-#from huggingface_hub import login
+from huggingface_hub import login
 
 #login(HUGGING_FACE_TOKEN)
 
 pipeline = AutoPipelineForImage2Image.from_pretrained(
     "kandinsky-community/kandinsky-2-2-decoder", torch_dtype=torch.float16, use_safetensors=True
 )
+
 #offload the model to the GPU while the other pipeline components wait on the CPU
 pipeline.enable_model_cpu_offload()
 # remove following line if xFormers is not installed or you have PyTorch 2.0 or higher installed
@@ -32,7 +35,7 @@ init_image = Image.open(image_path).convert("RGB")
 prompt = "With blonde hair, and smiling. The face must stay the same."
 
 # pass prompt and image to pipeline
-image = pipeline(prompt, image=init_image).images[0]
+image = pipeline(prompt, image=init_image, strength=0.1, guidance_scale=0.1).images[0]
 # Save the upscaled and resized image as a .webp file
 image.save("featured_image.webp", format="WEBP")
 #make_image_grid([init_image, image], rows=1, cols=2)
